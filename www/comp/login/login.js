72dc4b6a6263959ba2400e734591540a440c8cdb
gyanesh94 <gyanesh.cse@gmail.com>
2015-07-03 22:41:17 +0530

    Merge remote-tracking branch 'origin/develop' translation removed dynamic url base settings

--- www/comp/login/login.js
+++ www/comp/login/login.js
@@ -1,5 +1,51 @@
-ionic_app.controller('login_controller', function($scope, $state){
-    $scope.login_validation = function(){
-        $state.transitionTo('main.select_receipt');
+ionic_app.controller('login_controller', function ($scope, $state, $cordovaToast, $cordovaFile, switch_preffered_language, login_authentication, login_sid) {
+
+    $scope.login_object = {
+        username: '',
+        password: '',
+        disable: false,
+        full_name: ''
+    };
+    $scope.login = angular.copy($scope.login_object);
+
+    // Record Sid
+    $scope.record_sid = function (data) {
+        document.addEventListener('deviceready', function () {
+            $cordovaFile.writeFile(cordova.file.dataDirectory, "sid.txt", JSON.stringify(data), true)
+                .then(function (success) {}, function (error) {});
+        });
+    };
+
+
+    $scope.login_validation = function () {
+        $scope.login.disable = true;
+        login_authentication.login_authenticate($scope.login.username, $scope.login.password)
+            .success(function (data) {
+                $scope.login.disable = false;
+                sid = {
+                    sid: data.sid,
+                    name: data.full_name
+                };
+                login_sid.sid = sid.sid;
+                login_sid.name = sid.name
+                $scope.record_sid(sid);
+                delete $scope.login;
+                $scope.login = angular.copy($scope.login_object);
+                $state.transitionTo('main.select_receipt');
+            })
+            .error(function (data) {
+                $scope.login.disable = false;
+                if (data)
+                    if (data.message)
+                        $cordovaToast.show(data.message, 'short', 'bottom');
+            });
+    };
+
+    $scope.switch_to_english = function () {
+        switch_preffered_language.translate_language('en');
+    };
+
+    $scope.switch_to_hindi = function () {
+        switch_preffered_language.translate_language('hi');
     };
 });
--- www/comp/select_receipt/select_receipt.js
+++ www/comp/select_receipt/select_receipt.js
@@ -3,7 +3,8 @@ ionic_app.controller('select_receipt_controller', function ($scope, $state) {
     $scope.good_receipt_form = function () {
         $state.transitionTo('main.good_receipt.customer_name');
     };
+
     $scope.payment_receipt_form = function () {
-        $state.transitionTo('main.payment_receipt');
+        $state.transitionTo('main.payment_receipt.payment_receipt_information');
     };
 });
